f49ee842b30b24ad64c45ab05412155ff210925b
Damien <damiensabo@live.fr>
2021-10-13 20:32:14 +0200

    Small add in main.ts

--- main.ts
+++ main.ts
@@ -2,3 +2,5 @@ import MusicalPiece from './MusicalPiece';
 
 let mp = new MusicalPiece({ title: 'All my life', artist: 'Foo Fighters', album: 'One by One', releaseDate: new Date('2002-09-24') });
 mp.printMainInfo();
+mp.title = 'Times Like These';
+mp.printMainInfo();
